fcd5f7a6d75f6847b05b002894f85bea608c503a
ycheng11065 <ychengli11065@gmail.com>
2025-01-29 18:23:38 -0800

    created simple SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,33 @@
 #include <SDL.h>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World!"; 
-    std::cin.get();
+    SDL_Window* window = NULL;
+    SDL_Surface* winSurface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "Error initializing SDL:" << SDL_GetError() << std::endl;
+    }
+
+    window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::cerr << "Failed to create a window! Error: " << SDL_GetError() << std::endl;
+        system("pause");
+        return -1;
+    }
+
+    winSurface = SDL_GetWindowSurface(window);
+    
+    if (!winSurface) {
+        std::cerr << "Error getting surface! Error: " << SDL_GetError() << std::endl;
+        system("pause");
+        return -1;
+    }
+
+    system("pause");
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
