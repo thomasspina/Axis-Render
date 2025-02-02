481e984e65883e2eece594cc56073857552fe393
ycheng11065 <ychengli11065@gmail.com>
2025-02-02 14:35:51 -0800

    setting OpenGL attributes

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error initializing SDL:" << SDL_GetError() << std::endl;
     }
 
+    // Access to OpenGL 4.6
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    // Prevent screen flickering
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
 
     if (!window) {
@@ -26,6 +34,11 @@ int main(int argc, char* argv[]) {
         system("pause");
         return -1;
     }
+
+    SDL_GLContext main_context = SDL_GL_CreateContext(window);
+
+    // Enable V-Sync
+    SDL_GL_SetSwapInterval(1);
 
     while (!quit) {
 
